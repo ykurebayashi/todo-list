9398ff1fc8932f662b7bf395b42b059198437dc5
ykurebayashi <yngwie.rs2@gmail.com>
2022-02-10 17:35:56 -0300

    Local Storage fix
    - Fixing localstorage problem (overwriting the second user data)

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import GlobalContext from "../Context/GlobalContext";
 
 function Login() {
-    const { email, setEmail, userName, setUserName } = useContext(GlobalContext);
+    const { email, setEmail, userName, setUserName, setTempTodo, addTodo } = useContext(GlobalContext);
 
     const handleEmail = ({ target }) => {
         setEmail(target.value)
@@ -12,7 +12,28 @@ function Login() {
         setUserName(target.value)
     }
 
+    function submitToLocalStorage() {
+      const theLocal = JSON.parse(localStorage.getItem(`${userName}`));
 
+      if (theLocal === null) {
+        const userInfos = {
+          user: userName,
+          email: email,
+          todos: [],
+        };
+        console.log('fez certo')
+        localStorage.setItem(`${userName}`,JSON.stringify(userInfos));
+      } else {
+        console.log('falhou')
+      }
+    }
+
+    useEffect(() => {
+      setEmail('');
+      setUserName('');
+      setTempTodo('');
+      addTodo([]);
+    }, [])
 
     return (
         <div className="main-login">
@@ -28,8 +49,10 @@ function Login() {
                 <input type="text" placeholder="Type your username" value={userName} onChange={ (e) => handleUser(e)} />
               </section>
             </div>
-            <Link to="/todos" onClick={ () => console.log('vasco') } >
-              <button>
+            <Link to="/todos">
+              <button
+                onClick={ submitToLocalStorage }
+              >
                   Submit
               </button>
             </Link>
--- src/Pages/Todos.jsx
+++ src/Pages/Todos.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
+import { Link } from 'react-router-dom';
 import GlobalContext from "../Context/GlobalContext";
 
 function Todos() {
@@ -20,17 +21,43 @@ function Todos() {
         setTempTodo('');
     }
 
+    function logOut() {
+      setTempTodo('');
+      addTodo([]);
+    }
+
+    useEffect(() => { // pegando os do local storage e adicionando ao todos, que é a lista de todos os afazeres
+      const theLocalStorage = JSON.parse(localStorage.getItem(`${userName}`));
+      if (theLocalStorage === null) {
+        alert('Volte para tela de Login ou suas atividades não serão salvas!')
+      } else {
+        theLocalStorage.todos.map((todo) => {
+          addTodo(todos => [...todos, todo]);
+        })
+      }
+    }, []);
+
+    useEffect(() => {
+      const localStorageTodos = JSON.parse(localStorage.getItem(`${userName}`));
+      localStorageTodos.todos = todos
+      localStorage.setItem(`${userName}`, JSON.stringify(localStorageTodos));
+      setTempTodo('');
+    }, [todos]);
+
     return(
         <div className="todos-page">
           <header className="todos-header">
             <p>{`Olá, ${userName.charAt(0).toUpperCase() + userName.slice(1)}`}</p>
             <p>{`Email: ${email}`}</p>
+            <Link to="/" >
+              Logout
+            </Link>
           </header>
 
           <div className="todo-form">
             <input type="text" placeholder="Atividade" value={tempTodo} onChange={ (e) => setNewTempTodo(e) } />
             <button
-            onClick={ addTheTodo }
+              onClick={ addTheTodo }
             >
               Add to list!
             </button>
